4276670aa2cf1e02c2d3791b9f4f6cd848fe2c6c
Anton <gidrontv1@gmail.com>
2023-06-25 15:26:28 +0400

    added in utils logger

---
+++ utils.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/logs.log')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
